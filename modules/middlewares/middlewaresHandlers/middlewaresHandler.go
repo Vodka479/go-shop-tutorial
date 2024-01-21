6d920300e5482fd4ae9fbc409bbf1f5622c37bfe
Vodka479 <songwut2015@hotmail.com>
2024-01-22 00:18:26 +0700

    -feature :sparkles: Role-Based Auth Updated

--- modules/middlewares/middlewaresHandlers/middlewaresHandler.go
+++ modules/middlewares/middlewaresHandlers/middlewaresHandler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Vodka479/go-shop-tutorial/modules/entities"
 	"github.com/Vodka479/go-shop-tutorial/modules/middlewares/middlewaresUsecases"
 	"github.com/Vodka479/go-shop-tutorial/pkg/goauth"
+	"github.com/Vodka479/go-shop-tutorial/pkg/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -18,6 +19,7 @@ const (
 	routerCheckErr middlewareHandlersErrCode = "middleware-001"
 	jwtAuthErr     middlewareHandlersErrCode = "middleware-002"
 	paramsCheckErr middlewareHandlersErrCode = "middleware-003"
+	authorizeErr   middlewareHandlersErrCode = "middleware-004"
 )
 
 type IMiddlewaresHandler interface {
@@ -26,6 +28,7 @@ type IMiddlewaresHandler interface {
 	Logger() fiber.Handler
 	JwtAuth() fiber.Handler
 	ParamsCheck() fiber.Handler
+	Authorize(expectRoleId ...int) fiber.Handler
 }
 
 type middlewaresHandler struct {
@@ -109,5 +112,49 @@ func (h *middlewaresHandler) ParamsCheck() fiber.Handler {
 			).Res()
 		}
 		return c.Next()
+	}
+}
+
+func (h *middlewaresHandler) Authorize(expectRoleId ...int) fiber.Handler { /*expectRoleId รับมาเป็น Array / ...int รับไปเรื่อยๆ ไม่จำกัด*/
+	return func(c *fiber.Ctx) error {
+		userRoleId, ok := c.Locals("userRoleId").(int)
+		if !ok {
+			entities.NewResponse(c).Error(
+				fiber.ErrUnauthorized.Code,
+				string(authorizeErr),
+				"user_id is not int type",
+			).Res()
+		}
+		roles, err := h.middlewaresUsecase.FindRole()
+		if err != nil {
+			return entities.NewResponse(c).Error(
+				fiber.ErrInternalServerError.Code,
+				string(authorizeErr),
+				err.Error(),
+			).Res()
+		}
+
+		sum := 0
+		for _, v := range expectRoleId {
+			sum += v
+		}
+		// นำมา convert เป็น binary เพื่อนำไป bitwise operator
+		expectedValueBinary := utils.BinaryConverter(sum, len(roles))
+		userValueBinary := utils.BinaryConverter(userRoleId, len(roles))
+
+		// user -> 0 1 0
+		// expected -> 1 1 0
+
+		for i := range userValueBinary {
+			if userValueBinary[i]&expectedValueBinary[i] == 1 {
+				return c.Next()
+			}
+		}
+
+		return entities.NewResponse(c).Error(
+			fiber.ErrUnauthorized.Code,
+			string(authorizeErr),
+			"no permission to access",
+		).Res()
 	}
 }
